88dae587e4e503799ade9e9311b75626942d9ee6
Banu Darius-Matei <30901594+TheNumber5@users.noreply.github.com>
2023-08-12 15:52:34 +0300

    Update Autodecrypt.bat

--- Autodecrypt.bat
+++ Autodecrypt.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Run this to auto decrypt the images from the repository
-rem https://github.com/TheNumber5/c-onetimepad
+rem https://github.com/TheNumber5/onetimepad-c
 
 onetimepad -d example1encrypted.bmp example1result.bmp example1key.bin
 onetimepad -d example2encrypted.bmp example2result.bmp example2key.bin
